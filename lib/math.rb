f03da310f0548cb3ad3820a16466c20538559f4b
impartial-genre-2648 <vgscholz@gmail.com>
2020-06-30 03:42:01 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  puts "sum"
+  sum = num1 + num2
 end
 
 def subtraction(num1, num2)
-
+  puts "minus"
+  minus = num1 - num2
 end
 
 def division(num1, num2)
-
+  puts "divide"
+  divide = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  puts "multiply"
+  multiply = num1 * num2
 end
 
 def modulo(num1, num2)
-
+  puts "rem"
+  rem = num1.remainder(num2)
 end
 
 def square_root(num)
-
+  puts "srt"
+  srt = Math.sqrt(num)
 end
